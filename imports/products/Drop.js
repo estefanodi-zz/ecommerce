d80086a876bdc09e2759eaf4227d88e54f554372
estefanodi <estefanodi2009@gmail.com>
2018-07-17 13:08:55 +0200

    Update Drop.js

--- imports/products/Drop.js
+++ imports/products/Drop.js
@@ -16,18 +16,15 @@ export default class  DropdownExamplePointing extends React.Component{
   }
   
 
-  render(){
-    
-  	return(
-
-
-  <Menu compact style = {{fontSize:'1em'}}>
-    
-    <Dropdown text='Sort By'  className='link item' style={{
-    	                                                           width:'200px',
-    	                                                           textAlign:'center',
-                                                                   fontSize : '1em'
-                                                                  }}>
+render(){
+return(
+    <Menu compact style = {{fontSize:'1em'}}>
+       <Dropdown text='Sort By'  className='link item' 
+	         style={{
+		   width:'200px',
+		   textAlign:'center',
+		   fontSize : '1em'
+		  }}>
       <Dropdown.Menu>       
           <Dropdown.Item
                onClick = { () => this.props.changeSort() }
@@ -41,18 +38,18 @@ export default class  DropdownExamplePointing extends React.Component{
                   <Dropdown.Divider/>
         <Dropdown.Item>
           <Dropdown text='Category'  style={{width:'100%'}}>
-				            <Dropdown.Menu>
-                               <Dropdown.Item>All Categories</Dropdown.Item>
-                               <Dropdown.Divider/>
-				              {
-				              	this.state.categories.map( ( ele,i ) => {
-				              		return <Dropdown.Item
-                                                    key     = { i }
-                                                    onClick = { () => this.props.categorySort(ele.category)}
-				              		>{ele.category}</Dropdown.Item>
-				              	})
-				              }
-				            </Dropdown.Menu>
+        <Dropdown.Menu>
+	       <Dropdown.Item>All Categories</Dropdown.Item>
+	       <Dropdown.Divider/>
+	      {
+		this.state.categories.map( ( ele,i ) => {
+			return <Dropdown.Item
+		    key     = { i }
+		    onClick = { () => this.props.categorySort(ele.category)}
+			>{ele.category}</Dropdown.Item>
+		})
+	      }
+	    </Dropdown.Menu>
           </Dropdown>
 
         </Dropdown.Item>
