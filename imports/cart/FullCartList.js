5470ae9e50f329858f4458848974e724c65400a8
estefanodi <estefanodi2009@gmail.com>
2018-07-17 12:26:01 +0200

    Update FullCartList.js

--- imports/cart/FullCartList.js
+++ imports/cart/FullCartList.js
@@ -2,25 +2,22 @@ import React        from 'react'
 import FullCartItem from './FullCartItem'
 
 export default class FullCartList extends React.Component{
-	render(){
-		return(<div>{
-                      this.props.cart.map( (ele,i) => {
-                                   
-                      	                       	return  <FullCartItem 
-                                             className = 'fullCartList'
-                                             key       = { i }
-                                             product   = { ele }
-                                             removeFromCart = { this.props.removeFromCart }
-                                             updateCart     = { this.props.updateCart }
-                                             productId = { ele._id }
-                                             changeQuantity = { this.props.changeQuantity }
-                                            
-                                />
-                      })
-
-
-		       }</div>
-               
-			   )
-	}
+render(){
+    return(<div>
+	   {
+	      this.props.cart.map( (ele,i) => {
+		return  <FullCartItem 
+			     className = 'fullCartList'
+			     key       = { i }
+			     product   = { ele }
+			     removeFromCart = { this.props.removeFromCart }
+			     updateCart     = { this.props.updateCart }
+			     productId      = { ele._id }
+			     changeQuantity = { this.props.changeQuantity }
+			 />
+	      })
+		}
+	   </div>
+    )
+  }
 }
