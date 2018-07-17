dbbd2af516ea5ff210d246cb41d7d7c8073f533e
estefanodi <estefanodi2009@gmail.com>
2018-07-17 12:38:32 +0200

    Update App.js

--- imports/App.js
+++ imports/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 import Main from './Main'
 
 export default class App extends React.Component{
-   
-   render(){
-   	
-   	return(
-           <div>
-                <Main/>
-           </div>
+render(){
+   return(
+        <div>
+             <Main/>
+        </div>
           )
     }
 }
